a1e5988b3287f154cc001ec2f44dcf0027da2e6d
calvin-paumier <calvin.paumier@hotmail.fr>
2019-11-04 21:48:01 +0100

    commit

--- Exo2/Solar.py
+++ Exo2/Solar.py
@@ -15,5 +15,17 @@ class Solar:
         if (d20 == 1):
             return 0
         else :
-            if ((d20 + 35 > _ac) or (d20 == 20)):
+            if (d20 == 20):
+                return (2*(random.randint(1,6) + random.randint(1,6) + random.randint(1,6) + 18))
+            if (d20 + 35 > _ac):
                 return (random.randint(1,6) + random.randint(1,6) + random.randint(1,6) + 18)
+
+    def ranged_attack(self, _ac):
+        d20 = random.randint(1,20)
+        if (d20 == 1):
+            return 0
+        else :
+            if (d20 == 20):
+                return (2*(random.randint(1,6) + random.randint(1,6) + 14))
+            if (d20 + 35 > _ac):
+                return (random.randint(1,6) + random.randint(1,6) + 14)

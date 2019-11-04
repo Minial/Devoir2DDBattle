1a19dcb891e1b33cc2d7e75761dafcd27997c3cb
Minial <minet.adrien@free.fr>
2019-11-04 15:29:30 -0500

    first ground
    all your base are belong to us

---
+++ Terrain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 14:27:38 2019
+
+@author: adrien
+"""
+
+from WorgRider import WorgRider
+from Solar import Solar
+from Warlord import Warlord
+from Barbare import Barbare
+
+def Terrain :
+    tailleX = 1000
+    tailleY = 1000
+    listeMobs = []
+    
+    def __init__():
+        listeMobs.append(Solar(500,250))
+    
+        

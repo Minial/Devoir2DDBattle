98c3167f1e85b1ec8b0b4bbdb5912d13b460a3da
Minial <minet.adrien@free.fr>
2019-11-04 15:45:57 -0500

    moddif
    there be dragon

--- Exo2/Terrain.py
+++ Exo2/Terrain.py
@@ -11,12 +11,20 @@ from Solar import Solar
 from Warlord import Warlord
 from Barbare import Barbare
 
-def Terrain :
+class Terrain :
     tailleX = 1000
     tailleY = 1000
     listeMobs = []
     
     def __init__():
         listeMobs.append(Solar(500,250))
+        print (listeMobs[0].life)
+        
+        for mob in listeMobs:
+            if mob.life<=0 :
+                del listeMobs[listeMobs.index(mob)]
+        
+
+            
     
         

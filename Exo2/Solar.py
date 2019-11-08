7195f74b4d3e5ff476556507619ac6bd8d282196
calvin-paumier <calvin.paumier@hotmail.fr>
2019-11-08 15:52:59 +0100

    commit

--- Exo2/Solar.py
+++ Exo2/Solar.py
@@ -15,7 +15,7 @@ class Solar:
         self.listAllies = _la
         self.listEnnemie = _le
 
-    def melee_attack(self, _ac):
+    def melee_attack(self, _ac): # Renvoie les dégats que fait le Solar
         d20 = random.randint(1,20)
         if (d20 == 1):
             return 0
@@ -25,7 +25,7 @@ class Solar:
             if (d20 + 35 > _ac):
                 return (random.randint(1,6) + random.randint(1,6) + random.randint(1,6) + 18)
 
-    def ranged_attack(self, _ac):
+    def ranged_attack(self, _ac): # Renvoie les dégats que fait le Solar
         d20 = random.randint(1,20)
         if (d20 == 1):
             return 0
@@ -35,35 +35,40 @@ class Solar:
             if (d20 + 31 > _ac):
                 return (random.randint(1,6) + random.randint(1,6) + 14)
 
-    def action(self, _listeMob):
+    def action(self):
         listeDistance = []
-        for i in range(len(_listeMob)):
-            dx = self.position[0] - _listeMob[i].position[0]
-            dy = self.position[1] - _listeMob[i].position[1]
+        for i in range(len(self.listEnnemie)): # Calcul des distance avec les ennemies
+            dx = self.position[0] - self.listEnnemie[i].position[0]
+            dy = self.position[1] - self.listEnnemie[i].position[1]
             l = np.sqrt(dx*dx + dy*dy)
-            listeDistance.append([l - self.size/2 -_listeMob[i].size/2, l])
+            listeDistance.append([l - self.size/2 -self.listEnnemie[i].size/2, l])
 
-        minDist = min(listeDistance)
-        if (minDist[0] <= self.rangeMelee):
+        minDist = min(listeDistance) # Distance minimum
+
+        if (minDist[0] <= self.rangeMelee): # Distance inférieur à l'attque melee on ne se déplace pas
             destination = self.position
             
-        elif (minDist[0] <= self.speed + self.rangeMelee):
-            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
-            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
+        elif (minDist[0] <= self.speed + self.rangeMelee): # Déplacement en range melee (possibilité d'attaque)
+            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
+            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
             destination = [x,y]
-        elif (minDist[0] <= self.speed + self.rangeRanged):
-            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeRanged)*(_listeMob(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
-            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeRanged)*(_listeMob(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
+
+        elif (minDist[0] <= self.speed + self.rangeRanged): # Déplacement en range distance (possibilité d'attaque)
+            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeRanged)*(self.listEnnemie(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
+            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeRanged)*(self.listEnnemie(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
             destination = [x,y]
-        elif (minDist[0] <= 2*self.speed + self.rangeMelee):
-            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
-            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
+
+        elif (minDist[0] <= 2*self.speed + self.rangeMelee): # Déplacement en range melee (impossible d'attaquer)
+            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
+            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
             destination = [x,y]
-        elif (minDist[0] <= 2*self.speed + self.rangeRanged):
-            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
-            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(_listeMob(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
+
+        elif (minDist[0] <= 2*self.speed + self.rangeRanged): # Déplacement en range distance (impossible d'attaquer)
+            x = self.position[0] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[0] - self.position[0]) / minDist[1]
+            y = self.position[1] + (minDist[0] + self.size/2 - self.rangeMelee)*(self.listEnnemie(listeDistance.index(minDist)).position[1] - self.position[1]) / minDist[1]
             destination = [x,y]
-        else :
-            x = 2*self.speed*(_listeMob(listeDistance.index(minDist)).position[0] - self.position[0])/minDist[1]
-            y = 2*self.speed*(_listeMob(listeDistance.index(minDist)).position[1] - self.position[1])/minDist[1]
+
+        else : # Déplacement max
+            x = 2*self.speed*(self.listEnnemie(listeDistance.index(minDist)).position[0] - self.position[0])/minDist[1]
+            y = 2*self.speed*(self.listEnnemie(listeDistance.index(minDist)).position[1] - self.position[1])/minDist[1]
             destination = [x,y]

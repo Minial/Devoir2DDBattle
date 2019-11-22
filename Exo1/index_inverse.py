2cd5af526e8dad31bbf2b03c9318d2806138a44e
AxelPontet <axel.pontet@telecomnancy.eu>
2019-11-22 09:57:05 -0500

    index inverse

---
+++ Exo1/index_inverse.py
@@ -0,0 +1,25 @@
+import pymongo
+from bs4 import BeautifulSoup
+from liste_sorts import get_all_spells
+
+def index_inverse(sort):
+
+    res = []
+    all_spells = get_all_spells()
+    if(sort not in all_spells):
+        print("Ce sort n'existe pas")
+        return(None)
+    
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["DD_Creatures"]
+    col = db["Creatures"]
+    
+    myquery = { "spells": { "$ne": [] } }
+    mydoc = col.find(myquery, {"_id":0, "name":1, "spells":1})
+    for x in mydoc:
+      if (sort in x.get("spells")):
+          res.append(x.get("name"))
+    return(res)
+      
+print(index_inverse("haste"))
+
--- Exo1/liste_sorts.py
+++ Exo1/liste_sorts.py
@@ -1,21 +1,31 @@
 import pymongo
 from bs4 import BeautifulSoup
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client["DD_Creatures"]
-col = db["Creatures"]
+"""
+Fonction qui retourne tous les sorts du jeu
+Utile pour l'index inverse et pour l'interface
+"""
+
+def get_all_spells():
+
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["DD_Creatures"]
+    col = db["Creatures"]
 
 
-all_spells = []
-   
-myquery = { "spells": { "$ne": [] } }
-
-mydoc = col.find(myquery, {"_id":0, "spells":1})
-
-for x in mydoc:
-  for k in x.get("spells"):
-      if (k not in all_spells):
-          all_spells.append(k)
-
-print(all_spells)
-print(len(all_spells))
+    all_spells = []
+       
+    myquery = { "spells": { "$ne": [] } }
+    
+    mydoc = col.find(myquery, {"_id":0, "spells":1})
+    
+    for x in mydoc:
+      for k in x.get("spells"):
+          if (k not in all_spells):
+              all_spells.append(k)
+    
+    #print(all_spells)
+    #print(len(all_spells))
+    return(all_spells)
+    
+    client.close()
